eee1bddf4c8fef5c0507b2ddca6f9a06be48bbb3
Gabriel <27121228+gonzalezgabriel3@users.noreply.github.com>
2025-07-31 16:05:43 -0500

    Add getComputerChoice Function
    This Function generates a random number, the only possibilities are 0, 1 and 2
    and returns the word "Paper", "Rock" or "Scissors" depending on the number generated

--- javascript.js
+++ javascript.js
@@ -1,1 +1,37 @@
-console.log("Hello");
+//create function getComputerChoice
+//This function will return "rock", "paper" or "scissors"
+
+function getComputerChoice(){
+    let choice = (Math.floor(Math.random() * 3));
+    
+
+    switch (choice) {
+        case 0:
+            return "Rock";
+            break;
+
+        case 1:
+            return "Paper";
+            break;    
+
+        case 2:
+            return "Scissors";
+            break;
+    }
+        
+}
+
+console.log(getComputerChoice());
+
+
+//Create function getHumanChoice
+//getHumanChoice returns the user input
+
+
+
+//Create 2 variables to keep both the computer's score and the player's score
+//Those two variables initialize to 0
+//create function called playRound with parameters for humanChoice and computerChoice
+//humanChoice is case insensitive
+// playRound should output a string such as "You lose! Paper beats Rock"
+//Increment score of the winner
